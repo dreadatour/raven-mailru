61697720714776ab83ccf992990e4020ab914c4f
Vladimir Rudnyh <rudnyh@corp.mail.ru>
2012-10-03 18:43:10 +0400

    Stupid mistype

--- raven_mailru/processors.py
+++ raven_mailru/processors.py
@@ -51,7 +51,7 @@ class SanitizeMpopProcessor(Processor):
                 data[n]['Cookie'] = '; '.join('='.join(k) for k in bits)
 
             if 'Authorization' in data[n]:
-                if data[n]['Authorization'].lower().starswith('basic '):
+                if data[n]['Authorization'].lower().startswith('basic '):
                     data[n]['Authorization'] = 'Basic ********'
 
     def process(self, data, **kwargs):
